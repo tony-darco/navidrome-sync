4578e138b03c44f4daa42700435ffbeb9f1161b0
tony-darco <tonydarco40@gmail.com>
2026-04-20 01:08:10 -0400

    Add sync_to_xcode.sh script for syncing iOS files

---
+++ sync_to_xcode.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copies all iOS source files from navidrome-sync to the Xcode project.
+set -e
+
+SRC="/Users/tdarco/Documents/Projects/navidrome-sync/navidrome-ios"
+DST="/Users/tdarco/Documents/Projects/navidrome/navidrome-ios/navidrome-ios"
+
+# Use rsync to mirror the entire directory, preserving structure.
+# --delete removes files in DST that no longer exist in SRC.
+rsync -av --delete \
+  --include='*.swift' \
+  --include='*.plist' \
+  --include='*.json' \
+  --include='*/' \
+  --exclude='*' \
+  "$SRC/" "$DST/"
+
+echo "Done – synced all files from navidrome-sync to Xcode project."
